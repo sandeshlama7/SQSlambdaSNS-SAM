a9506f5d8ccc038b813cc2013e6090cf45da9ec8
Sandesh Lama <sandesh@ip-10-10-1-139.ec2.internal>
2024-07-19 09:40:20 +0545

    Added sns subscription

--- .aws-sam/build/SamLambdaSandesh/lambda_function.py
+++ .aws-sam/build/SamLambdaSandesh/lambda_function.py
@@ -3,15 +3,9 @@ import boto3, os
 sns = boto3.client('sns')
 
 def lambda_handler(event,context):
-  try:
     response = sns.publish(
       TopicArn= os.environ['SNS_ARN'],
-      Message="An image wass uploaded to the S3 bucket"
+      Message="An image was uploaded to the S3 bucket"
       )
     print("Message published")
     return response
-
-  except Exception e:
-    print(e)
-    print("Error publishing message")
-    return e
